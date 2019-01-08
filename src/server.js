0bd89c6ea6962a5d35cb18049c7f92d5a5401368
alexandre <alexandre.robert57@gmail.com>
2019-01-08 09:32:02 +0100

    start

---
+++ src/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('../public'));
+
+app.use('/', require('./routes/route'));
+
+app.set("view engine", "twig");
+app.set("views", "./src/views");
+
+app.listen(3000, function () {
+    console.log('Express app listening on port 3000');
+});
