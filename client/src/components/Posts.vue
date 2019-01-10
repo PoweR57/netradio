f867793abd85470a816550baffbff1e7c2bc45b7
alexandre <alexandre.robert57@gmail.com>
2019-01-10 14:03:42 +0100

    test

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -10,24 +10,28 @@
                 <span>{{ post.description }}</span>
             </p>
         </div>
-        <button onclick="asPresenter()">Presenter</button>
-        <button onclick="asStreamer()">Streamer</button>
-        <input type="text" id="debug">
-        <br>
-        <p>Presenter must be online before the streamer</p>
+        <button v-on:click="asPresenter()">Presenter</button>
+        <button v-on:click="asStreamer()">Streamer</button>
+        <input type="text" id="debug"><br>
+        Presenter must be online before the streamer
     </div>
 </template>
 
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js"></script>
-<script type="text/javascript" src="/dist/SFMediaStream.min.js"></script>
+<script type="text/javascript" src="@/static/SFMediaStream.min.js"></script>
 <script>
 /* eslint-disable */
+// import ScarletsMediaPresenter from "@/services/SFMediaStream";
 import PostsService from "@/services/PostsService";
+import io from 'socket.io-client';
+
 export default {
     name: "posts",
     data() {
         return {
-            posts: []
+            posts: [],
+            socket : io('localhost:8081'),
+            mySocket: io("/", {transports:['websocket']}),
+            debug: document.querySelector('#debug'),
         };
     },
     mounted() {
@@ -37,62 +41,55 @@ export default {
         async getPosts() {
             const response = await PostsService.fetchPosts();
             this.posts = response.data;
-        }
-    }
-};
-var mySocket = io("/", { transports: ["websocket"] });
-
-var debug = document.querySelector("#debug");
-function asPresenter() {
-    // Set latency to 100ms (Equal with streamer)
-    var presenterMedia = new ScarletsMediaPresenter(
-        {
-            audio: {
-                channelCount: 1,
-                echoCancellation: false
-            }
         },
-        100
-    );
+        asPresenter: function() {
+			// Set latency to 100ms (Equal with streamer)
+			var presenterMedia = new ScarletsMediaPresenter({
+			    audio:{
+			        channelCount:1,
+			        echoCancellation: false
+			    }
+			}, 100);
 
-    presenterMedia.onRecordingReady = function(arrayBuffer) {
-        console.log("Recording started!");
-        console.log("Header size: " + arrayBuffer.byteLength);
+			presenterMedia.onRecordingReady = function(arrayBuffer){
+			    console.log("Recording started!");
+			    console.log("Header size: " + arrayBuffer.byteLength);
 
-        // Every new client streamer must receive this header buffer data
-        mySocket.emit("bufferHeader", {
-            data: arrayBuffer,
-            mimeType: presenterMedia.options.mimeType
-        });
-    };
+			    // Every new client streamer must receive this header buffer data
+			    this.mySocket.emit('bufferHeader', {
+			        data:arrayBuffer,
+			        mimeType:presenterMedia.options.mimeType
+			    });
+			}
 
-    presenterMedia.onBufferProcess = function(streamData) {
-        debug.value = "Buffer sent: " + streamData[0].byteLength + "bytes";
-        mySocket.emit("stream", streamData);
-    };
+			presenterMedia.onBufferProcess = function(streamData){
+				debug.value = "Buffer sent: "+streamData[0].byteLength+"bytes";
+			    this.mySocket.emit('stream', streamData);
+			}
 
-    presenterMedia.startRecording();
-}
+			presenterMedia.startRecording();
+        },
+        asStreamer: function() {
+			// Set latency to 100ms (Equal with presenter)
+			audioStreamer = new ScarletsAudioBufferStreamer(3, 100);
+			audioStreamer.playStream();
 
-var audioStreamer = false;
-function asStreamer() {
-    // Set latency to 100ms (Equal with presenter)
-    audioStreamer = new ScarletsAudioBufferStreamer(3, 100);
-    audioStreamer.playStream();
+			// Buffer header must be received first
+			this.mySocket.on('bufferHeader', function(packet){
+				audioStreamer.mimeType = packet.mimeType;
+			    audioStreamer.setBufferHeader(packet.data);
+			});
 
-    // Buffer header must be received first
-    mySocket.on("bufferHeader", function(packet) {
-        audioStreamer.mimeType = packet.mimeType;
-        audioStreamer.setBufferHeader(packet.data);
-    });
+			// Receive buffer and play it
+			this.mySocket.on('stream', function(packet){
+				debug.value = "Buffer received: "+packet[0].byteLength+"bytes";
+			    audioStreamer.realtimeBufferPlay(packet);
+			});
 
-    // Receive buffer and play it
-    mySocket.on("stream", function(packet) {
-        debug.value = "Buffer received: " + packet[0].byteLength + "bytes";
-        audioStreamer.realtimeBufferPlay(packet);
-    });
+			// Request buffer header
+			this.mySocket.emit('requestBufferHeader', '');
+        }
+    },
 
-    // Request buffer header
-    mySocket.emit("requestBufferHeader", "");
-}
+};
 </script>
