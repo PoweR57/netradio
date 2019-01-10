4ab1b5557ac8b30174f8fa5f0419c2f4574062e1
alexandre <alexandre.robert57@gmail.com>
2019-01-10 12:38:59 +0100

    save

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -10,9 +10,16 @@
                 <span>{{ post.description }}</span>
             </p>
         </div>
+        <button onclick="asPresenter()">Presenter</button>
+        <button onclick="asStreamer()">Streamer</button>
+        <input type="text" id="debug">
+        <br>
+        <p>Presenter must be online before the streamer</p>
     </div>
 </template>
 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js"></script>
+<script type="text/javascript" src="/dist/SFMediaStream.min.js"></script>
 <script>
 /* eslint-disable */
 import PostsService from "@/services/PostsService";
@@ -33,4 +40,59 @@ export default {
         }
     }
 };
+var mySocket = io("/", { transports: ["websocket"] });
+
+var debug = document.querySelector("#debug");
+function asPresenter() {
+    // Set latency to 100ms (Equal with streamer)
+    var presenterMedia = new ScarletsMediaPresenter(
+        {
+            audio: {
+                channelCount: 1,
+                echoCancellation: false
+            }
+        },
+        100
+    );
+
+    presenterMedia.onRecordingReady = function(arrayBuffer) {
+        console.log("Recording started!");
+        console.log("Header size: " + arrayBuffer.byteLength);
+
+        // Every new client streamer must receive this header buffer data
+        mySocket.emit("bufferHeader", {
+            data: arrayBuffer,
+            mimeType: presenterMedia.options.mimeType
+        });
+    };
+
+    presenterMedia.onBufferProcess = function(streamData) {
+        debug.value = "Buffer sent: " + streamData[0].byteLength + "bytes";
+        mySocket.emit("stream", streamData);
+    };
+
+    presenterMedia.startRecording();
+}
+
+var audioStreamer = false;
+function asStreamer() {
+    // Set latency to 100ms (Equal with presenter)
+    audioStreamer = new ScarletsAudioBufferStreamer(3, 100);
+    audioStreamer.playStream();
+
+    // Buffer header must be received first
+    mySocket.on("bufferHeader", function(packet) {
+        audioStreamer.mimeType = packet.mimeType;
+        audioStreamer.setBufferHeader(packet.data);
+    });
+
+    // Receive buffer and play it
+    mySocket.on("stream", function(packet) {
+        debug.value = "Buffer received: " + packet[0].byteLength + "bytes";
+        audioStreamer.realtimeBufferPlay(packet);
+    });
+
+    // Request buffer header
+    mySocket.emit("requestBufferHeader", "");
+}
 </script>
--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-
 const app = express()
+const server = require('http').createServer(app);
+
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
@@ -18,4 +19,32 @@ app.get('/posts', (req, res) => {
     )
 })
 
-app.listen(process.env.PORT || 8081)
+// app.listen(process.env.PORT || 8081)
+
+
+var io = require('socket.io')(server);
+io.on('connection', () => { /* … */ });
+server.listen(process.env.PORT || 8081);
+
+// Every new streamer must have the buffer header from the presenter
+var bufferHeader = null;
+
+// Event listener
+io.on('connection', function (socket) {
+    /* Presenter */
+    socket.on('bufferHeader', function (packet) {
+        // Buffer header can be saved on server so it can be passed to new user
+        bufferHeader = packet;
+        socket.broadcast.emit('bufferHeader', packet);
+    });
+
+    // Broadcast the received buffer
+    socket.on('stream', function (packet) {
+        socket.broadcast.emit('stream', packet);
+    });
+
+    // Send buffer header to new user
+    socket.on('requestBufferHeader', function () {
+        socket.emit('bufferHeader', bufferHeader);
+    });
+});
